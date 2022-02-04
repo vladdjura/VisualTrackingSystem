2aec3cf7a1cec17d39532a25088cf61bdd21ec59
vladdjura <75100165+vladdjura@users.noreply.github.com>
2022-02-04 10:43:07 -0500

    creating complete parking frame status image

--- models.py
+++ models.py
@@ -1,12 +1,20 @@
 import cv2
 import numpy as np
 
-class Video:    
+class Video:
+    
+    with open('mask.npy', 'rb') as f:
+        mask = np.load(f)
+        
+    space_nums = [(1, 995, 760, 3, 4),(3, 1140, 760, 3, 4),(5, 838, 760, 3, 4),(7, 1284, 760, 3, 4), (9, 700, 760, 3, 4),
+              (2, 1018, 350, 1, 4),(4, 1110, 350, 1, 4),(6, 924, 350, 1, 4),(8, 1194, 350, 1, 4), (10, 826, 350, 1, 4),
+              (11, 1390, 760, 3, 4), (13, 540, 760, 3, 4), (14, 744, 350, 1, 4), (12, 1260, 350, 1, 4)]
+    
     def __init__(self, path):
         self.path = path
         self.cap = cv2.VideoCapture(self.path)
         self.frame = 1
-        self.frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))           
         
         print(f'Number of frames: {self.frames}')
         
@@ -44,6 +52,89 @@ class Video:
                 break
         self.cap.release()
         cv2.destroyAllWindows()
+
+    @property
+    def show_mask(self):
+        frame = self.read
+        frame[self.mask > 0] = np.array([200, 0, 100])
+        self.space_ids(frame)
+        cv2.imshow(f'frame', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
+    def save_mask(self, path):
+        frame = self.read
+        frame[self.mask > 0] = np.array([200, 0, 100])
+        self.space_ids(frame)
+        cv2.imwrite(path, frame)
+        
+    @property    
+    def ids(self):
+        frame = self.read
+        self.space_ids(frame)
+        cv2.imshow(f'frame', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+    @property
+    def var(self):
+        frame = self.read
+        variances = {}
+        for space_id in range(1,15):
+            space = frame[self.mask == space_id]
+            variances[space_id] = np.var(space)
+        return variances
+
+    # draw numbers on spaces
+    def space_ids(self, frame):
+        for space in self.space_nums:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            org = (space[1], space[2])
+            fontScale = space[3]
+            color = (210, 210, 210)
+            thickness = space[4]
+            frame = cv2.putText(frame, str(space[0]), org, font, 
+                               fontScale, color, thickness, cv2.LINE_AA)
+    
+    #paste variances on imige
+    def paste_var(self, frame, variances):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 1
+        thickness = 2
+        frame = cv2.putText(frame, 'Color varinaces', (100, 100), font, fontScale, (0,0,0), thickness, cv2.LINE_AA)
+        frame = cv2.circle(frame, (1640, 100), 15, (0,255,0), -1)
+        frame = cv2.circle(frame, (1640, 150), 15, (0,0,255), -1)
+        frame = cv2.putText(frame, 'Unoccupied', (1690, 110), font, fontScale, (0,255,0), thickness, cv2.LINE_AA)
+        frame = cv2.putText(frame, 'Occupied', (1690, 160), font, fontScale, (0,0,255), thickness, cv2.LINE_AA)
+        for space_id, var in variances.items():          
+            org = (100, space_id*50 + 100)
+            if var > 1500:
+                color = (0, 0, 255)
+            else:
+                color = (0, 255, 0)
+            phrase = f'{space_id}: {int(var)}'
+            frame = cv2.putText(frame, phrase, org, font, 
+                               fontScale, color, thickness, cv2.LINE_AA)
+                       
+   
+    @property    
+    def show_var(self):
+        frame = self.read
+        variances = self.var
+        self.paste_var(frame, variances)
+        self.space_ids(frame)
+        cv2.imshow(f'frame', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+        
+    def save_var(self, path):
+        frame = self.read
+        variances = self.var
+        self.paste_var(frame, variances)
+        self.space_ids(frame)
+        cv2.imwrite(path, frame)
+   
     
     # Create video with space availability text
     def texter(self, start, stop, mask, save_path, space_num = 1, variance = 1000):
